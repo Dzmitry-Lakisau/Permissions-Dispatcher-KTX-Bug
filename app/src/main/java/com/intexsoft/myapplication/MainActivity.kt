9ff4c84bea276484ff8b65356420af8352bc3a0a
Dzmitry Lakisau <laki_dimon@mail.ru>
2020-10-18 19:14:11 +0300

    Rewritten in ktx-way

--- app/src/main/java/com/intexsoft/myapplication/MainActivity.kt
+++ app/src/main/java/com/intexsoft/myapplication/MainActivity.kt
@@ -7,36 +7,35 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import kotlinx.android.synthetic.main.activity_main.*
-import permissions.dispatcher.NeedsPermission
-import permissions.dispatcher.RuntimePermissions
+import permissions.dispatcher.ktx.constructPermissionsRequest
 
-@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val cameraPermissionRequest = constructPermissionsRequest(Manifest.permission.CAMERA) {
+			openCamera()
+		}
+		val storagePermissionRequest = constructPermissionsRequest(Manifest.permission.READ_EXTERNAL_STORAGE) {
+			openFileManager()
+		}
+
 		button_camera.setOnClickListener {
-			openCameraWithPermissionCheck()
+			cameraPermissionRequest.launch()
 		}
 		button_file_manager.setOnClickListener {
-			openFileManagerWithPermissionCheck()
+			storagePermissionRequest.launch()
 		}
 	}
 
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-		onRequestPermissionsResult(requestCode, grantResults)
-	}
-
-	@NeedsPermission(Manifest.permission.CAMERA)
-	fun openCamera() {
+	private fun openCamera() {
 		val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, Environment.DIRECTORY_DOWNLOADS + "/temp.png")
 		startActivityForResult(intent, 1)
 	}
 
-	@NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-	fun openFileManager() {
+	private fun openFileManager() {
 		val intent = Intent().setAction(Intent.ACTION_GET_CONTENT)
 		startActivityForResult(Intent.createChooser(intent, "Choose file"), 2)
 	}
